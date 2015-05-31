b7acf417997afca100f04ed11bf2e6d6c5c67c14
Mariano <mariano.aquino@gmail.com>
2015-05-31 17:13:48 -0300

    add method to get all people in the db

--- app.js
+++ app.js
@@ -55,7 +55,32 @@ app.post("/person", jsonParser, function (req, res) {
     res.send(req.body);    // echo the result back
 });
 
-
+// get all people
+app.get("/person", jsonParser, function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+            res.sendStatus(400);
+        } else {
+            console.log('Connection established to', url);
+            // Get the documents collection
+            var collection = db.collection('person');
+            // find all people
+            collection.find({}).toArray(function(err, result) {
+                if (err) {
+                    console.log(err);
+                } else if (result.length) {
+                    console.log('Found:', result);
+                    // res.send(result)
+                } else {
+                    console.log('No document(s) found with defined "find" criteria!');
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
+});
 
 app.listen(port);
 
